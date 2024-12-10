de8a9206da714f627ecd93c5b44544c6a5af1f93
Taxiarchis Paterelis <taxiarxhspit05@gmail.com>
2024-12-10 00:47:50 +0000

    Update Queue.c with the completed code

--- Queue.c
+++ Queue.c
@@ -1,90 +1,123 @@
 #include <stdio.h>
 #define SIZE 10
 
+// Definition of the non dynamic queue object.
 typedef struct {
 	int front;
 	int rear;
-	int tbl[SIZE];
+	int arr[SIZE];
 } Queue;
 
-void init_queue(Queue *s) {
-	s->front = s->rear = -1;
-}
-int QueueIsEmpty(Queue *s) {
-	if ((s->front) == (s->rear) && (s->front) == -1) {
-		printf("The Queue is empty! ");
-		return 1;
-	} else return 0;
+// Function that initialises the queue.
+void initQueue(Queue *q) {
+	q->front = q->rear = -1; //The front & rear indicators are set as -1 to indicate an empty queue.
 }
 
-int QueueIsFull(Queue *s) {
-	if (s->rear == SIZE-1) {
-		printf("The Queue is full! ");
-		return 1;
-	}
-	else return 0;
+// Function that checks whether a non dynamic queue is empty.
+int QueueIsEmpty(Queue *q) {
+	if ((q->front) == (q->rear) && (q->front) == -1) {
+		// Since the queue is being implemented using a non dynamic structure - a table -
+		//if the indicator for the position of top or the rear is equal to -1, the queue must be empty.
+		printf("The queue is empty! ");
+		return 1; // Logical true.
+	} else return 0; // Logical false.
 }
 
-int QueueSize(Queue *s) {
-	if (QueueIsEmpty(s)) {
-		return 0;
-	} else if (QueueIsFull(s)) {
-		return SIZE;
-	}
-	return s->rear - s->front;
-}
-int Enqueue(Queue *s, int num) {
-	if (QueueIsFull(s)) {
-		printf("Enqueueing of %d failed!", num);
-		return -1;
-	}
-	
-	(s->rear)++;
-	s->tbl[s->rear] = num;
-	return 1;
+// Function that checks whether a non dynamic queue is full.
+int QueueIsFull(Queue *q) {
+	if (q->rear == SIZE-1) {
+		// Since the queue is being implemented using a non dynamic structure - a table -
+		//if the indicator for the position of the rear is equal to SIZE - 1, the queue must be full.
+		printf("The queue is full! ");
+		return 1; // Logical true.
+	} else return 0; // Logical false.
 }
 
-int Dequeue(Queue *s) {
-	if (QueueIsEmpty(s)) {
-		printf("Dequeueing failed!");
-		return -1;
-	} else if ((s->front) + 1 == s->rear) {
-		int num = s->tbl[s->front++];
-		s->front = s->rear = -1;
+// Function that pushes a provided integer to the queue, if possible.
+int Enqueue(Queue *q, int num) {
+	if (QueueIsFull(q)) {
+		// If the queue is full, pushing into it is impossible.
+		printf("Enqueueing of %d failed!\n", num);
+		return -1; // Error indicator.
 	}
 	
-	(s->front)++;
-	int num = s->tbl[(s->front)];
-	return num;
+	(q->rear)++; // Increase of the rear indicator.
+	q->arr[q->rear] = num; // Saving of the pushed number on the data structure.
+	return 1; // Process completed successfully.
 }
 
-int First(Queue *s) {
-	if (QueueIsEmpty(s)) {
-		printf("There is no first number!");
-		return -1;
+// Function that pop an integer from the queue in the position "front" if possible.
+int Dequeue(Queue *q) {
+	if (QueueIsEmpty(q)) {
+		// If the queue is full, poping from it is impossible.
+		printf("Dequeueing failed!\n");
+		return -1; // Error indicator.
+	// Checking whether the queue has only one integer inside,
+	// meaning that removing that integer empties it.
+	} else if ((q->front) + 1 == q->rear) {
+		int num = q->arr[q->front++];
+		q->front = q->rear = -1;
 	}
 	
-	return s->tbl[s->front++];
+	(q->front)++; // Increase of the front indicator
+	
+	int num = q->arr[(q->front)]; // Reading and returning
+	return num; // the integer that was removed from the queue.
 }
 
-int Last(Queue *s) {
-	if (QueueIsEmpty(s)) {
-		printf("There is no first number!");
-		return -1;
+// Function that returns
+int First(Queue *q) {
+	// Ensuring that the queue has at least one integer before attempting to read from it.
+	if (QueueIsEmpty(q)) {
+		printf("There is no first member in the queue!\n");
+		return -1; // Error indicator.
 	}
 	
-	return s->tbl[s->rear];
+	return q->arr[q->front++];
 }
 
+int Last(Queue *q) {
+	// Ensuring that the queue has at least one integer before attempting to read from it.
+	if (QueueIsEmpty(q)) {
+		printf("There is no last member in the queue!\n");
+		return -1; // Error indicator.
+	}
+	
+	return q->arr[q->rear];
+}
+
+int QueueSize(Queue *q) {
+	return q->rear - q->front;
+	// In any case of the current implementation, the correct size will be outputed by the
+	// above calculation. 
+}
+
+void PrintQueue(Queue *q) {
+	// Ensuring that the queue has at least one integer before attempting to print its contents.
+	if (QueueIsEmpty(q)) {
+		printf("There is nothing to print in the queue!\n");
+		return;	
+	}
+	
+	int i;
+	// Printing of the queue's contents
+	for (i=(q->front)+1; i<=(q->rear); i++) {
+		printf("|%d|", q->arr[i]);
+	}
+	printf("\n");
+}
+
+// Example code containing the usage for all functions.
 int main(void) {
-	Queue p;
+	Queue q;
 	int num;
-	num = Dequeue(&p);
-	Enqueue(&p,5);
-	Enqueue(&p,3);
-	Enqueue(&p,4);
-	printf("%d", First(&p));
-	printf("%d", Last(&p));	
-	num = Dequeue(&p);
-	num = Dequeue(&p);
+	num = Dequeue(&q);
+	Enqueue(&q,5);
+	Enqueue(&q,3);
+	Enqueue(&q,4);
+	printf("%d", First(&q));
+	printf("%d", Last(&q));
+	PrintQueue(&q);
+	num = Dequeue(&q);
+	num = Dequeue(&q);
 }
